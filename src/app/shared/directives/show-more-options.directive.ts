4b02b4f4e68f0a416ce09242c44be2b522741d6c
michal <m.pietrucha@techocean.pl>
2020-11-04 15:15:21 +0100

    add transition side-menu-element

--- src/app/shared/directives/show-more-options.directive.ts
+++ src/app/shared/directives/show-more-options.directive.ts
@@ -1,20 +1,23 @@
-import {Directive, ElementRef, HostListener} from '@angular/core';
+import {AfterViewChecked, Directive, ElementRef, HostListener, OnInit} from '@angular/core';
 
 @Directive({
     selector: '[appShowMoreOptions]'
 })
-export class ShowMoreOptionsDirective {
+export class ShowMoreOptionsDirective implements AfterViewChecked {
 
-    constructor(private el: ElementRef) {
+    constructor(private el: ElementRef) {}
+
+    ngAfterViewChecked() {
+        if (this.el.nativeElement.firstChild.classList.contains('text-info')) {
+            // this.el.nativeElement.lastChild.style.height = 'auto'
+        }
     }
 
-    @HostListener('click', ['$event']) onClick($event) {
-        const isActive = this.el.nativeElement.lastChild.style.height == 'auto'
-        if (!isActive) {
-            this.el.nativeElement.lastChild.style.height = 'auto'
-        } else {
-            this.el.nativeElement.lastChild.style.height = '0'
-        }
+    @HostListener('click') onClick() {
+        const isActive = this.el.nativeElement.lastChild.style.height.replace(/\D/g, '');
+        console.log(isActive == '');
+        this.el.nativeElement.lastChild.style.height = isActive == '0' || isActive == '' ?
+            this.el.nativeElement.lastChild.children[0].clientHeight * this.el.nativeElement.lastChild.children.length + 'px' : '0vh';
 
     }
 
